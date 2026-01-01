4bd42b088055854eccd734cc368b98c45b1f5c68
Lochard <110186560+locharp@users.noreply.github.com>
2026-01-01 12:30:13 +0000

    Create 13. Challenge 3: Fibonacci Sequence.py

---
+++ freeCodeCamp/Daily Coding Challenges/2025/August/13. Challenge 3: Fibonacci Sequence.py
@@ -0,0 +1,14 @@
+def fibonacci_sequence \
+(
+    start_sequence \
+    , length
+):
+    
+    while \
+    (
+        len( start_sequence ) < length
+    ):
+        start_sequence.append( sum( start_sequence[-2:] ) )
+    
+    
+    return start_sequence[:length]
