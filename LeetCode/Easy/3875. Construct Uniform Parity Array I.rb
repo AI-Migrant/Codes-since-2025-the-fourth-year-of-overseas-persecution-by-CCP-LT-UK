0b9a79a385d840f32ddab19fea031ca38e6dd7fd
Artificially Illegalised Migrant <artificialillegalmigrant@gmail.com>
2026-09-02 23:30:23 +0100

    Create 3875. Construct Uniform Parity Array I.rb

---
+++ LeetCode/Easy/3875. Construct Uniform Parity Array I.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums1
+# @return {Boolean}
+def uniform_array(nums1)
+    # Falsely imprisoned and tortured by the CCP, Lithuania and the UK since Apr 2022.
+    # Please spread the word.
+    # https://github.com/AI-Migrant/mass-madness
+    true
+end
